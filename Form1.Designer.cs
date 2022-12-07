4716be9a76fa09ec063888e35674711e41fbd35f
attila832558 <ubikutya2003@gmail.com>
2022-12-07 23:42:57 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace FoldrajzGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(460, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 26);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Adatbazis letrehozasa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(460, 33);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 27);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Adatbazis torlese";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label1.Location = new System.Drawing.Point(12, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Add meg az adatbazis nevet:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox1.Location = new System.Drawing.Point(219, 83);
+            this.textBox1.MaxLength = 32;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(226, 23);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "foldrajz";
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(460, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 24);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "SQL fajl beillesztese";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label2.Location = new System.Drawing.Point(12, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Feladatok megoldasa:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(16, 333);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(746, 199);
+            this.listBox1.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Szerver:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label4.Location = new System.Drawing.Point(12, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Port:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label5.Location = new System.Drawing.Point(233, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Nev:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label6.Location = new System.Drawing.Point(233, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 20);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Jelszo:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox2.Location = new System.Drawing.Point(71, 9);
+            this.textBox2.MaxLength = 32;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(156, 23);
+            this.textBox2.TabIndex = 17;
+            this.textBox2.Text = "localhost";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox3.Location = new System.Drawing.Point(290, 9);
+            this.textBox3.MaxLength = 32;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(155, 23);
+            this.textBox3.TabIndex = 18;
+            this.textBox3.Text = "root";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox4.Location = new System.Drawing.Point(71, 44);
+            this.textBox4.MaxLength = 32;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(156, 23);
+            this.textBox4.TabIndex = 19;
+            this.textBox4.Text = "3306";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox5.Location = new System.Drawing.Point(290, 38);
+            this.textBox5.MaxLength = 32;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(155, 23);
+            this.textBox5.TabIndex = 20;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label7.Location = new System.Drawing.Point(12, 535);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(185, 20);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Egyéni parancs lefuttatása:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox6.Location = new System.Drawing.Point(16, 558);
+            this.textBox6.MaxLength = 32;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(583, 23);
+            this.textBox6.TabIndex = 22;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(596, 558);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(76, 23);
+            this.button7.TabIndex = 23;
+            this.button7.Text = "Start";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(668, 558);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(94, 23);
+            this.button8.TabIndex = 24;
+            this.button8.Text = "Doboz kiüríése";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FoldrajzGUI.Properties.Resources.mysql_5_logo_png_transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(600, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(162, 113);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 25;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(774, 601);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Adatbazis kezelo program";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FoldrajzGUI
+{
+    public partial class Form1 : Form
+    {
+        Button[] gombok;
+        public Form1()
+        {
+            InitializeComponent();
+
+            int width = this.ClientSize.Width;
+            int height = this.ClientSize.Height;
+            int wCount = width / 70;
+
+            gombok = new Button[65];
+
+            for (int i = 0; i < gombok.Length; i++)
+            {
+                gombok[i] = new Button();
+                gombok[i].Location = new Point(i % wCount * 70, (i / wCount) * 25 + 150);
+                gombok[i].Name = (i + 1).ToString();
+                gombok[i].Size = new Size(70, 25);
+                gombok[i].Text = (i + 1) + ". feladat".ToString();
+                gombok[i].UseVisualStyleBackColor = true;
+
+                gombok[i].Click += GombKattintas;
+
+                this.Controls.Add(gombok[i]);
+            }
+
+            foreach (var item in File.ReadAllLines("feladatok.txt"))
+            {
+                feladatok.Add(item);
+            }
+        }
+
+        public static List<string> feladatok = new List<string>();
+
+        private void GombKattintas(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            try
+            {
+                string connStr = $"server={textBox2.Text};user={textBox3.Text};port={textBox4.Text};password={textBox5.Text}";
+                MySqlConnection conn = new MySqlConnection(connStr);
+                conn.Open();
+                string sql_useDatabase = $"use {textBox1.Text}";
+                MySqlCommand cmd_useDatabase = new MySqlCommand(sql_useDatabase, conn);
+                cmd_useDatabase.ExecuteNonQuery();
+                string sql_select = feladatok[int.Parse(b.Name)-1];
+                MySqlCommand cmd = new MySqlCommand(sql_select, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                listBox1.Items.Clear();
+                listBox1.Items.Add(b.Text);
+                listBox1.Items.Add("Ez a parancs futott le:");
+                listBox1.Items.Add(" ");
+                listBox1.Items.Add(sql_select);
+                listBox1.Items.Add(" ");
+                while (rdr.Read())
+                {
+                    for (int i = 0; i < rdr.FieldCount; i++)
+                    {
+                        listBox1.Items.Add(rdr[i]);
+                    }
+                }             
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Hiba tortent", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) // letrehozas
+        {
+            try
+            {
+                string connStr = $"server={textBox2.Text};user={textBox3.Text};port={textBox4.Text};password={textBox5.Text}";
+                MySqlConnection conn = new MySqlConnection(connStr);
+                conn.Open();
+                string sql_createDatabase = $"CREATE DATABASE {textBox1.Text} DEFAULT CHARACTER set utf8 COLLATE utf8_hungarian_ci;";
+                MySqlCommand cmd_createDatabase = new MySqlCommand(sql_createDatabase, conn);
+                cmd_createDatabase.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Az adatbazis sikeresen letrejott!", "Siker", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Hiba tortent", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) // torles
+        {
+            try
+            {
+                string connStr = $"server={textBox2.Text};user={textBox3.Text};port={textBox4.Text};password={textBox5.Text}";
+                MySqlConnection conn = new MySqlConnection(connStr);
+                conn.Open();
+                string sql_drop = $"drop database {textBox1.Text}";
+                MySqlCommand cmd_drop = new MySqlCommand(sql_drop, conn);
+                cmd_drop.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Az adatbazis sikeresen torolve lett!", "Siker", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Hiba tortent", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("A feladat hibatlan megoldasa miatt a szovegdoboz jelenleg csak olvashato!", "Figyelmeztetes", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void button1_Click(object sender, EventArgs e) // beillesztes
+        {
+            try
+            {
+                string connStr = $"server={textBox2.Text};user={textBox3.Text};port={textBox4.Text};password={textBox5.Text}";
+                MySqlConnection conn = new MySqlConnection(connStr);
+                conn.Open();
+                string sql_useDatabase = $"use {textBox1.Text}";
+                MySqlCommand cmd_useDatabase = new MySqlCommand(sql_useDatabase, conn);
+                cmd_useDatabase.ExecuteNonQuery();
+                string fileContent = string.Empty;
+                string filePath = string.Empty;
+                using (OpenFileDialog openFileDialog1 = new OpenFileDialog())
+                {
+                    openFileDialog1.InitialDirectory = "c:\\";
+                    openFileDialog1.Filter = "SQL Database (*.sql)|*.sql";
+                    openFileDialog1.FilterIndex = 1;
+                    openFileDialog1.RestoreDirectory = true;
+
+                    if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        filePath = openFileDialog1.FileName;
+                        var fileStream = openFileDialog1.OpenFile();
+                        using (StreamReader reader1 = new StreamReader(fileStream))
+                        {
+                            fileContent = reader1.ReadToEnd();
+                        }
+                        string sql_insert = File.ReadAllText(filePath);
+                        MySqlCommand cmd_insert = new MySqlCommand(sql_insert, conn);
+                        cmd_insert.ExecuteNonQuery();
+                        MessageBox.Show("Az SQL fajl sikeresen beillesztesre kerult!", "Siker", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        conn.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Hiba tortent", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e) // Custom Command
+        {
+            try
+            {
+                if (textBox6.Text.Length >= 1)
+                {
+                    try
+                    {
+                        string connStr = $"server={textBox2.Text};user={textBox3.Text};port={textBox4.Text};password={textBox5.Text}";
+                        MySqlConnection conn = new MySqlConnection(connStr);
+                        conn.Open();
+                        string sql_useDatabase = $"use {textBox1.Text}";
+                        MySqlCommand cmd_useDatabase = new MySqlCommand(sql_useDatabase, conn);
+                        cmd_useDatabase.ExecuteNonQuery();
+                        string sql_insert = $"{textBox6.Text}";
+                        MySqlCommand cmd = new MySqlCommand(sql_insert, conn);
+                        cmd.ExecuteNonQuery();
+                        listBox1.Items.Clear();
+                        listBox1.Items.Add("Ez a parancs futott le:");
+                        listBox1.Items.Add(" ");
+                        listBox1.Items.Add(sql_insert);
+                        MessageBox.Show("A feladat sikeresen lefutott!", "Siker", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Hiba tortent", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("A parancs nem lehet üres", "Hiba tortent", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Hiba tortent", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
